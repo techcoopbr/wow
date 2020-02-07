7d1902f7c7f748aa115d67dfaa494c292ebc525d
darlan <sdarlanset@gmail.com>
2020-02-07 10:21:04 -0300

    Mudancas das migrations necessarias para rodar db:migrate

--- db/migrate/20191018020159_create_game_creators.rb
+++ db/migrate/20191018020159_create_game_creators.rb
@@ -1,7 +1,6 @@
 class CreateGameCreators < ActiveRecord::Migration[5.0]
   def change
     create_table :game_creators do |t|
-      t.references :game, foreign_key: true
       t.references :creator, foreign_key: true
 
       t.timestamps
--- db/migrate/20200115181418_drop_game.rb
+++ db/migrate/20200115181418_drop_game.rb
@@ -1,6 +1,5 @@
 class DropGame < ActiveRecord::Migration[5.0]
   def change
     drop_table :game_creators
-    drop_table :games
   end
 end
