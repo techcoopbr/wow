c3ac4c87d7449699b798fd4d37f1b9ae79701501
Jefferson Speck <jeffersonspeck@msn.com>
2020-03-26 18:00:05 -0300

    acertos

--- db/migrate/20191018020159_create_game_creators.rb
+++ db/migrate/20191018020159_create_game_creators.rb
@@ -1,10 +1,11 @@
 class CreateGameCreators < ActiveRecord::Migration[5.0]
   def change
-    create_table :game_creators do |t|
-      t.references :game, foreign_key: true
-      t.references :creator, foreign_key: true
+      create_table :game_creators do |t|
+      #  t.references :game, foreign_key: true
+        t.references :creator, foreign_key: true
 
-      t.timestamps
-    end
+        t.timestamps
+
+      end
   end
 end
