bb6093b6fb49e2a543bfffbdf23a3da9f91ee04d
darlan <sdarlanset@gmail.com>
2020-04-02 15:36:29 -0300

    contagem de comentarios no painel

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -3,6 +3,17 @@ class PanelController < ApplicationController
   layout "application"
 
   def index
-    @creator = Creator.find_by(user_id: current_user.id)
+    @creator = Creator.find_by!(user_id: current_user.id)
+    
+    @blogs = @creator.blogs
+
+    @comments = Array.new
+    @anonymous_comments = Array.new
+
+    @blogs.each do |b|
+      @comments += b.comments
+      @anonymous_comments += b.anonymous_comments
+    end
+
   end
 end
