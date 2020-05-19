3413779e88ab1884fcc336965ecb8e0a8609d7bc
Darlan Selzlein <sdarlanset@gmail.com>
2020-05-19 19:11:11 -0300

    configurando scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,9 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(File.expand_path('../../scheduler.yml', __FILE__))
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  end
 end
 
 Sidekiq.configure_client do |config|
