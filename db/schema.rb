426bc3f9be1db8d9c235b67f11f796513b02bc0f
Darlan Selzlein <sdarlanset@gmail.com>
2020-05-20 16:55:51 -0300

    atualizado campo last_tweet_id pra ser bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200520144300) do
+ActiveRecord::Schema.define(version: 20200520194844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 20200520144300) do
     t.string   "meta_photo"
     t.string   "script_google"
     t.integer  "impressions_count"
-    t.integer  "last_tweet_id"
+    t.bigint   "last_tweet_id"
     t.index ["user_id"], name: "index_creators_on_user_id", using: :btree
   end
 
