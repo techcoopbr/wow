41bbe4ccd7e308c896e0e23dbe15d545dff4e878
Darlan Selzlein <sdarlanset@gmail.com>
2020-05-20 11:52:37 -0300

    adicionada coluna las_tweet_id em creators

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416131513) do
+ActiveRecord::Schema.define(version: 20200520144300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20200416131513) do
     t.string   "meta_photo"
     t.string   "script_google"
     t.integer  "impressions_count"
+    t.integer  "last_tweet_id"
     t.index ["user_id"], name: "index_creators_on_user_id", using: :btree
   end
 
