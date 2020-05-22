1af0da5f1287b561b3e7699206c1484227fa2fc4
Darlan Selzlein <sdarlanset@gmail.com>
2020-05-22 16:31:22 -0300

    changes in models/game.rb to raise errors

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,26 +1,10 @@
 class Game < ApplicationRecord
+  validates :name, uniqueness: true
   has_many :game_developers
   has_many :game_publishers
   mount_uploader :photo, PhotoUploader
 
-  def add_developers(info_hash)
-    GameDeveloper.create_developer_relation(self, info_hash['developers'])
-  end
-
-  def add_publisher(info_hash)
-    GamePublisher.create_publisher_relation(self, info_hash['publishers'])
-  end
-
-  def set_steam_attributes(info_hash)
-    self.name =               info_hash['name']
-    self.source_id =          info_hash['steam_appid']
-    self.description =        info_hash['detailed_description']
-    self.about =              info_hash['about_the_game']
-    self.short_description =  info_hash['short_description']
-    self.source =             'steam'
-    self.add_developers(info_hash)
-    self.add_publisher(info_hash)
-  end
+  include Steam
 
   def self.create_or_update_steam_game(info_hash)
     game = Game.find_by(name: info_hash['name'])
@@ -39,6 +23,40 @@ class Game < ApplicationRecord
 
   def self.steam_game_request
     SteamApiRequestJob.perform_later
+  end
+
+  def self.steam_games_list
+    Steam::Client.new.app_list['applist']['apps']
+  end
+
+  def self.get_details(id)
+    client = Steam::StoreClient.new(id)
+    response = client.app_details
+
+    raise Steam::TooManyRequestsError.new if response.code == 429
+    
+    raise Steam::NilResponseError.new if response.nil?
+
+    response[id.to_s]['data']
+  end
+
+  def add_developers(info_hash)
+    GameDeveloper.create_developer_relation(self, info_hash['developers'])
+  end
+
+  def add_publishers(info_hash)
+    GamePublisher.create_publisher_relation(self, info_hash['publishers'])
+  end
+
+  def set_steam_attributes(info_hash)
+    self.name =               info_hash['name']
+    self.source_id =          info_hash['steam_appid']
+    self.description =        info_hash['detailed_description']
+    self.about =              info_hash['about_the_game']
+    self.short_description =  info_hash['short_description']
+    self.source =             'steam'
+    self.add_developers(info_hash)
+    self.add_publishers(info_hash)
   end
 
 end
