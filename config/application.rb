ce7df943f835bb65a6bd2df27cd509a88f8404ba
Jefferson Rodrigo Speck <jeffersonspeck@msn.com>
2020-06-09 22:16:52 -0300

    coisas

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,6 @@ module Wow
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.eager_load_paths << Rails.root.join('lib/twitch/client.rb')
 
     config.to_prepare do
         Devise::SessionsController.layout "login"
