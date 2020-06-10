c17dcb452f720a2cec803b6efbc6c0b56cdf3c9e
Jefferson Rodrigo Speck <jeffersonspeck@msn.com>
2020-06-09 22:20:34 -0300

    acertos

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,7 @@ module Wow
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.autoload_paths << Rails.root.join('lib')
 
     config.to_prepare do
         Devise::SessionsController.layout "login"
